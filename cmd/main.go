be5a0712aee446bf32d0a3aa676630732cbc3234
arya2004 <arya20j4@gmail.com>
2024-12-14 01:02:33 +0530

    Implement WebSocket channel handling and update server port

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/arya2004/gobanter/pkg/handlers"
 	"github.com/arya2004/gobanter/pkg/routes"
 )
 
 func main(){
 	mux := routes.Routes()
 
-	log.Println("starting on port 8083")
+	log.Println("starting channel listener")
+	go handlers.ListenToWsChannel()
 
-	_ = http.ListenAndServe(":8083", mux)
+	log.Println("starting on port 8080")
+
+	_ = http.ListenAndServe(":8080", mux)
 
 }
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,6 +8,11 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var wsChannel = make(chan WsPayload)
+
+var clients = make(map[*websocket.Conn]string)
+
+
 var views =	jet.NewSet(
 	jet.NewOSFileSystemLoader("./templates"),
 	jet.InDevelopmentMode(),
@@ -30,12 +35,20 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+
 type WsJsonResponse struct {
 	Action string `json:"action"`
 	Message string `json:"message"`
 	MessageType string `json:"message_type"`
 }
 
+type WsPayload struct {
+	Action string `json:"action"`
+	Username string `json:"username"`
+	Message string `json:"message"`
+	Conn *websocket.Conn `json:"-"`
+}
 
 
 func WsEndpoint(w http.ResponseWriter, r* http.Request){
@@ -51,10 +64,61 @@ func WsEndpoint(w http.ResponseWriter, r* http.Request){
 	var response WsJsonResponse
 	
 	response.Message = `<em><small>connected to server</small></em>`
+
+	clients[ws] = ""
+
+
 	err = ws.WriteJSON(response)
 	
 	if err != nil {
 		log.Panicln(err)
+	}
+
+	go ListenForWs(ws)
+}
+
+func ListenToWsChannel(){
+	var response WsJsonResponse
+	
+	for {
+		e := <- wsChannel
+		response.Action = "Got here"
+		response.Message = e.Action
+		broadcastToAll(response)
+	}
+}
+func broadcastToAll(response WsJsonResponse){
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Printf("error: %v", err)
+			_ = client.Close()
+			delete(clients, client)
+		}
+	}
+}
+
+
+func ListenForWs(conn *websocket.Conn){
+	defer func(){
+		if r := recover(); r != nil {
+			log.Printf("error %v", r)
+		}
+	}()
+
+	var payload WsPayload
+	for {
+		
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			log.Println(err)
+			return
+		}else{
+			payload.Conn = conn
+			wsChannel <- payload
+		}
+
+		
 	}
 }
 
