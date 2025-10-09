e44bb1f1e734fc9045360f631c3c9e64b1f8b85f
Suthar345Piyush <piyushsuthar524@gmail.com>
2025-10-10 00:51:46 +0530

    Added the check for ws upgrader to prevent CSWH attacks

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -34,7 +34,10 @@ var (
 	upgradeConnection = websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
-		CheckOrigin:     func(r *http.Request) bool { return true },
+		CheckOrigin: func(r *http.Request) bool {
+			origin := r.Header.Get("Origin")
+			return origin == "http://localhost:8080"
+		},
 	}
 )
 
